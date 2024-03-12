906c5f9e072acb6f4b4ada0f8225ed274e350f61
DiyaPatel15 <diyavalam1508@gmail.com>
2024-03-12 16:12:57 +0530

    Login committed by diya patel on 12 march 2024

---
+++ account/constants.py
@@ -0,0 +1,34 @@
+EMPLOYEE_DESIGNATION = (
+    ("CEO","CEO"),
+    ("COO", "COO"),
+    ("Project Manager", "Project Manager"),
+    ("Jr. PHP Laravel Developer","Jr. PHP Laravel Developer"),
+    ("Sr. Developer","Sr. Developer"),
+    ("UI/UX Designer","UI/UX Designer"),
+    ("Product Manager","Product Manager"),
+    ("Quality Engineer","Quality Engineer"),
+    ("Quality Engineer Lead","Quality Engineer Lead"),
+    ("Web Designer","Web Designer"),
+    ("Sr. Developer","Sr. Developer"),
+    ("SEO Executive", "SEO Executive"),
+    ("Jr. HR Executive", "Jr. HR Executive"),
+    ("Jr. PHP Laravel Developer	", "Jr. PHP Laravel Developer"),
+    ("Sr. HR Executive", "Sr. HR Executive"),
+    ("Sr. SEO Executive", "	Sr. SEO Executive"),
+    ("Content Writer", "Content Writer"),
+    ("Python Developer", "Python Developer"),
+    ("Jr. Web Designer", "Jr. Web Designer"),
+    ("Intern", "Intern"),
+    ("Lead Generation Executive", "Lead Generation Executive"),
+    ("Trainee", "Trainee"),
+)
+EMPLOYEE_ROLE = (
+    ("Admin", "Admin"),
+    ("HR", "HR"),
+    ("Employee", "Employee"),
+)
+
+EMPLOYEE_COMPANY = (
+    ("PranshTech Solutions", "PranshTech Solutions"),
+    ("Textdrip", "Textdrip"),
+)
